494498eb716f6fc9a7551f9d71aa2e9dc389089b
Fernando Alberto Lerma Sarabia <fiernando_lerma@hotmail.com>
2022-01-10 20:54:59 -0700

    multipart simple

---
+++ WebApplication1/Controllers/FilesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers {
+    [Route("api/[controller]/")]
+    [ApiController]
+    public class FilesController : Controller {
+        //create
+        [HttpPost]
+        public IActionResult AddItem(IFormFile file) {
+            try {
+                return Ok(file);
+            } catch (Exception ex) {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
