357a5561e2e3684092c759901580cffdcfca3747
developer1holati <developer1.holati@outlook.com>
2022-01-11 18:39:11 -0700

    get file data

--- WebApplication1/Controllers/FilesController.cs
+++ WebApplication1/Controllers/FilesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Services;
+using WebApplication1.Models;
+using System.Text;
 
 namespace WebApplication1.Controllers {
     [Route("api/[controller]/")]
@@ -17,6 +19,58 @@ namespace WebApplication1.Controllers {
                 if (!AuthServices.ValidateToken(Request.Headers["Authorization"]))
                     return Unauthorized("Credenciales inválidas");
                 return Ok(FilesServices.Create(file,$@"{_env.ContentRootPath}files/").Result);
+            } catch (Exception ex) {
+                return StatusCode(StatusCodes.Status500InternalServerError,ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() {
+            try {
+                if (!AuthServices.ValidateToken(Request.Headers["Authorization"]))
+                    return Unauthorized("Credenciales inválidas");
+                return Ok(FilesServices.GetAll(new WhereSQL {
+                    SQLClauses = new string[] {
+                    }
+                }));
+            } catch (Exception ex) {
+                return StatusCode(StatusCodes.Status500InternalServerError,ex.Message);
+            }
+        }
+
+        [HttpGet("{id:long}")]
+        public IActionResult Get(long id) {
+            try {
+                if (!AuthServices.ValidateToken(Request.Headers["Authorization"]))
+                    return Unauthorized("Credenciales inválidas");
+                Files? item = FilesServices.GetSingle(new WhereSQL {
+                    SQLClauses = new string[] {
+                        $"{FilesSQLTable.id} = {id}"
+                    }
+                });
+                if (item == null) return NotFound(null);
+                return Ok(item);
+            } catch (Exception ex) {
+                return StatusCode(StatusCodes.Status500InternalServerError,ex.Message);
+            }
+        }
+
+        [HttpGet("content/{id:long}")]
+        public IActionResult GetContent(long id) {
+            try {
+                if (!AuthServices.ValidateToken(Request.Headers["Authorization"]))
+                    return Unauthorized("Credenciales inválidas");
+                Files? item = FilesServices.GetSingle(new WhereSQL {
+                    SQLClauses = new string[] {
+                        $"{FilesSQLTable.id} = {id}"
+                    }
+                });
+                if (item == null) return NotFound(null);
+                if (!System.IO.File.Exists(item.Path)) {
+                    if (item == null) return NotFound(null);
+                }
+
+                return File(FilesServices.GetFileBytes(item!.Path!).Result,FilesServices.GetMimeType(item?.Path?.Split(".")?.LastOrDefault()));
             } catch (Exception ex) {
                 return StatusCode(StatusCodes.Status500InternalServerError,ex.Message);
             }
